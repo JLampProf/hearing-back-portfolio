824db7db1bc32c93f0c5eba43ffa793b7a267b40
JCLamprecht <66572608+JCLamprecht@users.noreply.github.com>
2025-04-11 18:55:43 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+
+const submit = (e) => {
+  e.preventDefault();
+
+  const nameValue = nameInput.value;
+  const emailValue = emailInput.value; //Doesn't do anything
+  alert(
+    `Hi, ${nameValue}, don't worry, this form doesn't actually do anything.`
+  );
+};
+
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", submit);
